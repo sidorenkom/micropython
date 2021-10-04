95be0afcf8917c63098483de9e4366c9eeb28240
Mikhail Sidorenko <msidorenko@embedity.dev>
2021-10-04 18:52:44 +0300

    Correction of shortcomings in the code -Wall

--- ports/unix/mpconfigport.h
+++ ports/unix/mpconfigport.h
@@ -45,7 +45,7 @@
 // overriding defaults in py/mpconfig.h.
 
 // Variant-specific definitions.
-// __!!!__ #include "mpconfigvariant.h"
+#include "mpconfigvariant.h"
 
 // The minimal variant's config covers everything.
 // If we're building the minimal variant, ignore the rest of this file.
