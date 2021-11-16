1330164c1986fed358b6ea0fa7c870c4c8016cd1
sidorenkom <msidorenko@embedity.dev>
2021-11-16 15:25:11 +0300

    Mod STM32H7A3x folder

--- ports/stm32/boards/STM32H7A3x/mpconfigboard.mk
+++ ports/stm32/boards/STM32H7A3x/mpconfigboard.mk
@@ -1,20 +1,20 @@
 # MCU settings
-MCU_SERIES = f4
-CMSIS_MCU = STM32F469xx
+MCU_SERIES = h7
+CMSIS_MCU = STM32H7A3xxQ
 MICROPY_FLOAT_IMPL = double
-AF_FILE = boards/stm32f469_af.csv
+AF_FILE = boards/stm32h7a3x_af.csv
 
 ifeq ($(USE_MBOOT),1)
 # When using Mboot all the text goes together after the filesystem
-LD_FILES = boards/stm32f469xi.ld boards/common_blifs.ld
+LD_FILES = boards/stm32h7a3x.ld boards/common_blifs.ld
 TEXT0_ADDR = 0x08020000
 else ifeq ($(USE_DBOOT),1)
 # When using dual Bootloader all the text goes together after the filesystem
-LD_FILES = boards/stm32f469xi_dboot.ld boards/common_blifs.ld
+LD_FILES = boards/stm32h7a3x_dboot.ld boards/common_blifs.ld
 TEXT0_ADDR = 0x08020000
 else
 # When not using Mboot the ISR text goes first, then the rest after the filesystem
-LD_FILES = boards/stm32f469xi.ld boards/common_ifs.ld
+LD_FILES = boards/stm32h7a3x.ld boards/common_ifs.ld
 TEXT0_ADDR = 0x08000000
 TEXT1_ADDR = 0x08008000
 endif
